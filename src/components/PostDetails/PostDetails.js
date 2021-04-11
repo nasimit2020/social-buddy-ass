dde75824e1a19125e52e2fdbaeb70ff687d7ee5c
nasimit2020 <nasimitjesbd@gmail.com>
2021-04-11 21:54:54 +0600

    update button

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,16 +1,20 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './PostDetails.css'
 
 const PostDetails = (props) => {
     const {title, body, id} = props.post;
+    const history = useHistory();
+    const handleClick = (id) =>{
+        history.push(`/PostWithComt/${id}`)
+    }
     return (
         <div className="post-details">
             <h4>{title}</h4>
             <p>{body}</p>
-            <Link to={`/PostWithComt/${id}`}> Go to Id no: {id}</Link>
-            {/* <Button variant="contained" color="primary">Details Now</Button> */}
+            {/* <Link to={`/PostWithComt/${id}`}> Go to Id no: {id}</Link> */}
+            <Button onClick={() => handleClick(id)} variant="contained" color="primary">Click Me</Button>
         </div>
     );
 };
